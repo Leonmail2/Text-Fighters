89a0c6c922a9e5e7b9643447439cf40f6fd6ef9b
Liam Muir <muirliam23@gmail.com>
2019-05-16 11:15:28 -0400

    renamed combat data

--- combat_data.c
+++ combat_data.c
@@ -3,10 +3,16 @@
 
 #include "structs.h"
 
-int attack1table[3] = {};
-
-int attack2table[3] = {};
+struct attack attacktable[6] = {{3,2},{1,1},{1,3},{2,2},{5,-2},{1,3}}; //starts with fireball, goes to dagger, in up-down, left right order,
+//struct order is range, bonus
 
 int actable[3] = {11,14,12}; //based on numbers for each class offered on help string
+
+struct dice dicetable[6] = {{5,7},{3,5},{5,6},{2,4},{6,7},{2,5}};//starts with fireball, goes to dagger, in up-down, left right order
+
+struct dice gauntlets = {3,7}; //one off for gauntlets perk
+
+struct dice healingpotion = {4,8};
+
 
 #endif
