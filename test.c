6026e5454f5466ad8daebe9cc02d07f4ce168d61
Liam Muir <muirliam23@gmail.com>
2019-05-21 10:11:08 -0400

    file to test out functions

---
+++ test.c
@@ -0,0 +1,17 @@
+/*
+test.c
+
+Liam Muir, 2019
+
+a test file to ensure that functions are working as expected
+*/
+
+#include "core_logic.h"
+#include "combat_data.h"
+#include "initalize_players.h"
+#include "structs.h"
+
+int main(int argc, char const *argv[]){
+	
+	return 0;
+}
