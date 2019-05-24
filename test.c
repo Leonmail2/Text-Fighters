262b6d683dc2462c4b9971332dbbd7d1b35ade6a
Liam Muir <muirliam23@gmail.com>
2019-05-24 10:54:30 -0400

    deleted stuff inside test.c

--- test.c
+++ test.c
@@ -11,10 +11,6 @@ a test file to ensure that functions are working as expected
 #include "structs.h"
 
 int main(int argc, char const *argv[]){
-	struct match* p = initgame();
-	print_menu(1,p);
-	print_status(1,p);
-	print_menu(2,p);
-	print_status(2,p);
+	
 	return 0;
 }
